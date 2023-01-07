6798dbc063e5c71adf86c7884aac89d4fcc0ffe9
Nayem73 <nayem73@gmail.com>
2023-01-07 18:41:33 +0600

    55 leetcode jump game

---
+++ LeetCode/55_jump_game.cpp
@@ -0,0 +1,65 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	vector<int> nums = {3,2,1,0,4};
+	int n = nums.size();
+
+	int upto = 0;
+	vector<bool> vis(n);
+	vis[0] = true;
+
+
+	for (int i = 0; i < n; i++) {
+		if (!vis[i]) break;
+		if (upto >= i+nums[i]) continue;
+		else {
+			for (int j = upto+1; j <= i+nums[i]; j++) {
+				vis[j] = true;
+				if (j == n-1) {cerr<<"true"; return 0;}
+			}
+			upto = i+nums[i];
+		}
+	}
+	cerr<< "false";
+}
