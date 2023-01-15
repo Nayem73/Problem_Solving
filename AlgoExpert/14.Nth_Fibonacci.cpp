e63714170fa6b7c2f2393a652f3112aa4d5ca52e
Nayem73 <nayem73@gmail.com>
2023-01-15 20:26:13 +0600

    find 3 biggest in O(n).

---
+++ AlgoExpert/14.Nth_Fibonacci.cpp
@@ -0,0 +1,62 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int fib_rec(int n, int par) {
+	rje()<<rji(par)<<rji(n);
+	if (n == 1) return 1;
+	if (n == 0) return 0;
+	return fib_rec(n-1, n) + fib_rec(n-2, n);
+}
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	int n = 9;// cin >> n;
+	vector<int> fib(n+1);
+	fib[0] = 0;
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++) fib[i] = fib[i-1] + fib[i-2];
+	cerr << fib[n] << endl;
+	
+	//recursion way
+	cerr << fib_rec(n, -1)<<endl;
+
+}
