97d2f6e01740873f179e89e6507008fb48ce1e97
Nayem73 <nayem73@gmail.com>
2023-01-22 09:02:39 +0600

    weekly contest b

---
+++ LeetCode/6297.Sort_the_students_by_their_Kth_score.cpp
@@ -0,0 +1,66 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	vector<vector<int>> score {
+		{10,6,9,1},
+		{7,5,11,2},
+		{4,8,3,15}
+	};
+	int k = 2;
+	const int n = score.size();
+	vector<pair<int,int>> vp;
+	for (int i = 0; i < n; i++) {
+		vp.push_back({score[i][k], i});
+	}
+
+	sort(vp.rbegin(), vp.rend());
+
+	vector<vector<int>> out(n);
+	for (int i = 0; i < n; i++) {
+		int indx = vp[i].second;
+		for (int X: score[indx]) out[i].push_back(X);
+	}
+
+	rje()<<rji(out[0]);
+}
