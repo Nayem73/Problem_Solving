a7467943f0040b5045dda6e3336ffc3cbc0e1883
Nayem73 <nayem73@gmail.com>
2023-01-25 21:59:12 +0600

    Alhamdulillah

---
+++ others/codepanja_substring.cpp
@@ -0,0 +1,78 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+int t; cin >> t; while (t--) {
+
+	int n; cin >> n;
+	string v;
+	cin >> v;
+
+	string one, two;
+	vector<bool> vis(n);
+	one += v[0];
+	vis[0] = true;
+
+	for (int i = 1; i < n; i++) {
+		if (v[i] != one.back()) {
+			one += v[i];
+			vis[i] = true;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (!vis[i]) two += v[i];
+	}
+
+	int ans = one.size() - 1;
+	for (int i = 1; i < (int)two.size(); i++) {
+		if (two[i] != two[i-1]) ans++;
+	}
+
+	rje()<<rji(one) rji(two);
+
+	cout << ans << endl;
+
+
+}
+}
