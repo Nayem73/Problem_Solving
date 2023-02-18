a216d57332d724ef48fb2f657ce495e95aae082e
Nayem73 <nayem73@gmail.com>
2023-02-18 11:52:03 +0600

    why Runtime Error?

---
+++ LeetCode/226.Invert_Binary_Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    /*TreeNode* fnc(TreeNode* cur) {
+        if (!cur) return cur;
+        cur->right = fnc(cur->left);
+        cur->left = fnc(cur->right);
+        return cur;
+    }
+    */
+    TreeNode* invertTree(TreeNode* cur) {
+        //TreeNode *L = root->left;
+        //TreeNode *R = root->right;
+        
+        //return root;
+        //root->left = R;
+        //root->right = L;
+        
+        //return fnc(root);
+        //TreeNode cur = root;
+        if (!cur) return cur;
+        cout << cur->val<< endl;
+       // /*cur->right = */ invertTree(cur->left);
+        ///*cur->right = */ invertTree(cur->left);
+        cur->right = invertTree(cur->left);
+        cur->left = invertTree(cur->right);
+        return cur;
+        
+    }
+};
