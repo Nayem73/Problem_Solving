f92b19a0665a3bcc21348aabe5f4d75015ff4bb8
Nayem73 <nayem73@gmail.com>
2023-03-05 10:17:57 +0600

    3 overflow even in long long

---
+++ LeetCode/weekly_335/3.cpp
@@ -0,0 +1,66 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	vector<int> nums {4,7,8,15,3,5};
+	int n = nums.size();
+	vector<long long> pref(n);
+	pref[0] = nums[0];
+	for (int i = 1; i < n; i++) {
+		pref[i] = pref[i-1] * (long long)nums[i];
+	}
+	rje()<<rji(pref);
+
+	vector<long long> suff(n);
+	suff[n-1] = nums[n-1];
+	for (int i = n-2; i >= 0; i--) {
+		suff[i] = suff[i+1] * (long long) nums[i];
+	}
+	rje()<<rji(suff);
+
+	for (int i = 0; i < n-1; i++) {
+		if (__gcd(pref[i], suff[i+1]) == 1) {
+			cerr<<i<<endl;
+		}
+	}
+}
