b348b862bb9fb4e566d0e81dfcd57183d7433889
Nayem73 <nayem73@gmail.com>
2023-03-15 14:29:44 +0600

    Alhamdulillah handled edge case with ase++

--- LeetCode/958.Check_completeness_of_a_binary_tree.cpp
+++ LeetCode/958.Check_completeness_of_a_binary_tree.cpp
@@ -23,18 +23,31 @@ class Solution {
     
 public:
     bool isCompleteTree(TreeNode* root) {
-        traverse(root, 0);   
+        traverse(root, 0);
+        int lvl = -1;
+        for (auto X: store) {
+            if (X.empty()) break;
+            ++lvl;
+        }
+
         for (int i = 0; i <= 1000; i++) {
-            if (store[i].empty()) continue;
-            //cout << i << ": ";
-            //for (int X: store[i]) cout << X << ' ';
+            if (store[i].empty()) break;
+            cout << i << ": ";
+            for (int X: store[i]) cout << X << ' ';
             int n = store[i].size();
             bool nai = false;
+            int ase = 0;
             for (int j = 0; j < n; j++) {
                 if (nai && store[i][j] != -1) return false;
                 if (store[i][j] == -1) nai = true;
+                if (store[i][j] != -1) ase++;
             }
-            //cout << endl;
+            cout << endl;
+            if (!ase && i < lvl) return false;
+            if (i > 0 && i+1 < lvl) {
+                if (store[i+1][0] == -1) continue;
+                if (store[i-1].size()*2 != ase) return false;
+            }
         }
 
         return true;
