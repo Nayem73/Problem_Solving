5c76159dc76e8e7e32f5c2cf4074daf381c8734d
Nayem73 <nayem73@gmail.com>
2023-03-15 14:28:01 +0600

    again wrong, some interesting Edge Cases

--- LeetCode/958.Check_completeness_of_a_binary_tree.cpp
+++ LeetCode/958.Check_completeness_of_a_binary_tree.cpp
@@ -10,17 +10,33 @@
  * };
  */
 class Solution {
-    bool ans = true;
-    void traverse(TreeNode* cur) {
-        if (!cur) return;
-        if (!cur->left && cur->right) ans = false;
-        traverse(cur->left);
-        traverse(cur->right);
+    vector<int> store[1003];
+    void traverse(TreeNode* cur, int lvl) {
+        if (!cur) {
+            store[lvl].push_back(-1);
+            return;
+        }
+        store[lvl].push_back(cur->val);
+        traverse(cur->left, lvl+1);
+        traverse(cur->right, lvl+1);
     }
     
 public:
     bool isCompleteTree(TreeNode* root) {
-        traverse(root);   
-        return ans;
+        traverse(root, 0);   
+        for (int i = 0; i <= 1000; i++) {
+            if (store[i].empty()) continue;
+            //cout << i << ": ";
+            //for (int X: store[i]) cout << X << ' ';
+            int n = store[i].size();
+            bool nai = false;
+            for (int j = 0; j < n; j++) {
+                if (nai && store[i][j] != -1) return false;
+                if (store[i][j] == -1) nai = true;
+            }
+            //cout << endl;
+        }
+
+        return true;
     }
 };
