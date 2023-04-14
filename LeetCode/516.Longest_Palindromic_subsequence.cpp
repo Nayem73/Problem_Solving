3495051cbd2cf64da6a6386a0c6fb508219a7ffd
Nayem73 <nayem73@gmail.com>
2023-04-14 14:18:05 +0600

    Alhamdulillah

---
+++ LeetCode/516.Longest_Palindromic_subsequence.cpp
@@ -0,0 +1,70 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	string s = "bbbab";
+	int n = s.size();
+	vector<vector<int>> dp(n, vector<int> (n));
+  for (int len = 0; len < n; len++) {
+		for (int i = 0; i < n-len; i++) {
+			int j = i+len;
+			if (j >= n) break;
+			if (len == 0) dp[i][j] = 1;
+			else if (len == 1) {
+				if (s[i] == s[j]) dp[i][j] = 2;
+				else dp[i][j] = 1;
+			}
+			else if (s[i] == s[j]) {
+				dp[i][j] = dp[i+1][j-1] + 2;
+			}
+			else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+
+			rje()<< rji(i)rji(j) rji(dp[i][j]);
+		}
+  }
+
+  rje()<<rji(dp[0][n-1]);
+}
+
+
+ // cabadc
