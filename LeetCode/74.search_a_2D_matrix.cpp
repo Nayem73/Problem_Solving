f5a9cc00689996a46675b8f4e79f67bc10ae2565
Nayem73 <nayem73@gmail.com>
2023-08-07 15:26:23 +0600

    Alhamdulillah! beautiful solution! log(max(n,m))

---
+++ LeetCode/74.search_a_2D_matrix.cpp
@@ -0,0 +1,81 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	int n = 1, m = 1, target = 0;
+	vector<vector<int>> matrix(n, vector<int> (m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+
+	// rje()<<rji(matrix);
+
+	int lo = 0, hi = n-1, mid, got = 0;
+	while (lo <= hi) {
+		mid = (lo+hi)/2;
+		if (matrix[mid][0] <= target) {
+			got = mid;
+			lo++;
+		} else {
+			hi--;
+		}
+	}
+
+	rje()<<rji(got);
+	lo = 0, hi = m-1, mid = 0;
+	int got2 = -1;
+	while (lo <= hi) {
+		mid = (lo+hi)/2;
+		if (matrix[got][mid] <= target) {
+			got2 = matrix[got][mid];
+			lo++;
+		} else {
+			hi--;
+		}
+	}
+
+	cerr << got2 << endl;
+	got2 == target ? cerr << true : cerr << false;
+}
