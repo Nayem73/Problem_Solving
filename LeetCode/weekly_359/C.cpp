006996defe0904403ba3539534d0b96d8d548e1c
Nayem73 <nayem73@gmail.com>
2023-08-23 15:46:00 +0600

    wrong answer for test like Input vvvlo Output
    Expected
    vlvov

--- LeetCode/weekly_359/C.cpp
+++ LeetCode/weekly_359/C.cpp
@@ -1,58 +1,18 @@
-//BISMILLAHIR-RAHMANIR-RAHIM//
-#include <bits/stdc++.h>
-using namespace std;
-#define sim template < class c
-#define ris return * this
-#define dor > rje & operator <<
-#define eni(x) sim > typename \
-  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
-sim > struct rge { c b, e; };
-sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
-sim > auto dud(c* x) -> decltype(cerr << *x, 0);
-sim > char dud(...);
-struct rje {
-#ifdef LOCALM
-~rje() { cerr << endl; }
-eni(!=) cerr << boolalpha << i; ris; }
-eni(==) ris << range(begin(i), end(i)); }
-sim, class b dor(pair < b, c > d) {
-  ris << "(" << d.first << ", " << d.second << ")";
-}
-sim dor(rge<c> d) {
-  *this << "[";
-  for (auto it = d.b; it != d.e; ++it)
-    *this << ", " + 2 * (it == d.b) << *it;
-  ris << "]";
-}
-#else
-sim dor(const c&) { ris; }
-#endif
-};
-#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
-#define endl '\n'
-
-int main() {
-//ALHAMDULILLAHI-RABBIL-ALAMIN//
-#ifdef LOCALM
-freopen("in1", "r", stdin);
-freopen("out1", "w", stdout);
-#endif
-fast_io;
-//-------------------------------    
-    //SUBHANALLAH//
-//-------------------------------
-    int n = 3, k = 5;
-    map<int,int> mp;
-    int sum = 0;
-    int i = 1;
-    while (n--) {
-        // if (mp[i]) i++;
-        while (mp[i]) i++;
-        sum += i;
-        mp[k-i]++;
-        i++;
-    }
-
-    cerr<< sum;
-}
+          for (i = 0; i < 400; i++) { // Divide the delay into smaller intervals
+              delay_ms(50); // Delay for 50 ms
+              cnt_time += 50;
+              if (cnt_time == 500) {
+                if (portb.RB3 == 1) {
+                    portb.RB3 = 0;
+                }
+                else {
+                    portb.RB3 = 1;
+                }
+                cnt_time = 0;
+              }
+              if (portc.RC1 == 1) {
+                  portb.RB0 = 0; // Stop motor immediately
+                  portb.RB1 = 0;
+                  break; // Exit the loop
+              }
+          }
