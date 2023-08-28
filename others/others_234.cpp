43b37cbe31fb54017f976756fab7ca029db0312e
Nayem73 <nayem73@gmail.com>
2023-08-28 22:02:41 +0600

    .

---
+++ others/others_234.cpp
@@ -0,0 +1,39 @@
+output: {"status":"VALIDATED","tran_date":"2023-08-28 19:12:10","tran_id":"TRANS1693228318014_2758","val_id":"230828191256XZJZzgLZCvdoudm","amount":"500.00","currency":"BDT","bank_tran_id":"230828191256iJoOhsQUbSMitx6","card_type":"VISA-Dutch Bangla","card_no":"418117XXXXXX6675","card_issuer":"TRUST BANK, LTD.","card_brand":"VISA","card_category":"DEBIT","card_issuer_country":"Bangladesh","card_issuer_country_code":"BD","currency_type":"BDT","currency_amount":"500.00","emi_issuer":"TRUST BANK, LTD.","validated_on":"2023-08-28 19:12:57","card_ref_id":"dc1da4f52669828139e81ef5eb0f48a5a99ea054a131e00a562887d455417dd919",}
+
+
+
+
+
+
+{
+	tran_id=TRANS1693230509689_4918, 
+	val_id=23082819485211uc82uLrISOzbq, 
+	amount=500.00, 
+	card_type=VISA-Dutch Bangla, 
+	store_amount=487.50, 
+	card_no=432149XXXXXX0667, 
+	bank_tran_id=2308281948510JnWhgz2114RGkc, 
+	status=VALID, 
+	tran_date=2023-08-28 19:48:41, 
+	currency=BDT, 
+	card_issuer=BRAC BANK, LTD., 
+	card_brand=VISA, 
+	card_issuer_country=Bangladesh, 
+	card_issuer_country_code=BD, 
+	store_id=aifar64ea1f7a8c3ec, 
+	verify_sign=59c612504bacdcb9bd488f94ab059e14, 
+	verify_key=amount,
+	bank_tran_id,/
+	base_fair,card_brand,card_issuer,card_issuer_country,card_issuer_country_code,card_no,card_type,currency,currency_amount,currency_rate,currency_type,risk_level,risk_title,status,store_amount,store_id,tran_date,tran_id,val_id,value_a,value_b,value_c,value_d, verify_sign_sha2=165f3b83754d876f3c33978656cbea344ecfe683aaa920b54686ef8b03dde540, currency_type=BDT, currency_amount=500.00, currency_rate=1.0000, base_fair=0.00, value_a=ref00, value_b=ref00, value_c=ref00, value_d=ref00, risk_level=0, risk_title=Safe}
+
+
+
+
+
+
+// output: {"status":"VALIDATED","tran_date":"2023-08-28 19:14:39","tran_id":"TRANS1693228467691_6494","val_id":"230828191444d5qqI5CWaBBjMwK","amount":"500.00","store_amount":"487.5","currency":"BDT","bank_tran_id":"230828191444Dvn5p5PU5tr8Mtu","card_type":"QCASH-ITCL       ","card_no":"","card_issuer":"QCASH","card_brand":"QCASH","card_category":"QCASH","card_sub_brand":"","card_issuer_country":"Bangladesh","card_issuer_country_code":"BD","currency_type":"BDT","currency_amount":"500.00","currency_rate":"1.0000","base_fair":"0.00","value_a":"ref00","value_b":"ref00","value_c":"ref00","value_d":"ref00","emi_instalment":"0","emi_amount":"0.00","emi_description":"","emi_issuer":"QCASH","account_details":"","risk_title":"Safe","risk_level":"0","discount_percentage":"0","discount_amount":"0.00","discount_remarks":"","APIConnect":"DONE","validated_on":"2023-08-28 19:14:45","gw_version":"","offer_avail":1,"card_ref_id":"dc1da4f52669828139e81ef5eb0f48a5a99ea054a131e00a562887d455417dd919","isTokeizeSuccess":0,"campaign_code":""}
+
+// output: {"status":"VALIDATED","tran_date":"2023-08-28 19:15:12","tran_id":"TRANS1693228500133_8218","val_id":"230828191518kwmA2qf7sRJzeA7","amount":"500.00","store_amount":"487.5","currency":"BDT","bank_tran_id":"2308281915171ztWb5yMtalTSOQ","card_type":"BKASH-BKash      ","card_no":"","card_issuer":"BKash Mobile Banking","card_brand":"MOBILEBANKING","card_category":"MOBILE","card_sub_brand":"","card_issuer_country":"Bangladesh","card_issuer_country_code":"BD","currency_type":"BDT","currency_amount":"500.00","currency_rate":"1.0000","base_fair":"0.00","value_a":"ref00","value_b":"ref00","value_c":"ref00","value_d":"ref00","emi_instalment":"0","emi_amount":"0.00","emi_description":"","emi_issuer":"BKash Mobile Banking","account_details":"","risk_title":"Safe","risk_level":"0","discount_percentage":"0","discount_amount":"0.00","discount_remarks":"","APIConnect":"DONE","validated_on":"2023-08-28 19:15:18","gw_version":"","offer_avail":1,"card_ref_id":"dc1da4f52669828139e81ef5eb0f48a5a99ea054a131e00a562887d455417dd919","isTokeizeSuccess":0,"campaign_code":""}
+
+// output: {"status":"VALIDATED","tran_date":"2023-08-28 19:15:49","tran_id":"TRANS1693228537757_6679","val_id":"230828191553U3cLu0vnC94KeLO","amount":"500.00","store_amount":"487.5","currency":"BDT","bank_tran_id":"230828191553UV5pICQsBKfAmMv","card_type":"BANKASIA-Bank As ","card_no":"","card_issuer":"Bank Asia Limited","card_brand":"IB","card_category":"IB","card_sub_brand":"","card_issuer_country":"Bangladesh","card_issuer_country_code":"BD","currency_type":"BDT","currency_amount":"500.00","currency_rate":"1.0000","base_fair":"0.00","value_a":"ref00","value_b":"ref00","value_c":"ref00","value_d":"ref00","emi_instalment":"0","emi_amount":"0.00","emi_description":"","emi_issuer":"Bank Asia Limited","account_details":"","risk_title":"Safe","risk_level":"0","discount_percentage":"0","discount_amount":"0.00","discount_remarks":"","APIConnect":"DONE","validated_on":"2023-08-28 19:15:54","gw_version":"","offer_avail":1,"card_ref_id":"dc1da4f52669828139e81ef5eb0f48a5a99ea054a131e00a562887d455417dd919","isTokeizeSuccess":0,"campaign_code":""}
+// output: {"status":"VALIDATED","tran_date":"2023-08-28 19:16:37","tran_id":"TRANS1693228585166_3360","val_id":"230828191650imJdQPgjxll7EKO","amount":"500.00","store_amount":"487.5","currency":"BDT","bank_tran_id":"2308281916501ku1u6gHzPcvWiG","card_type":"CITYBANKIB-City B","card_no":"","card_issuer":"City Bank","card_brand":"IB","card_category":"IB","card_sub_brand":"","card_issuer_country":"Bangladesh","card_issuer_country_code":"BD","currency_type":"BDT","currency_amount":"500.00","currency_rate":"1.0000","base_fair":"0.00","value_a":"ref00","value_b":"ref00","value_c":"ref00","value_d":"ref00","emi_instalment":"0","emi_amount":"0.00","emi_description":"","emi_issuer":"City Bank","account_details":"","risk_title":"Safe","risk_level":"0","discount_percentage":"0","discount_amount":"0.00","discount_remarks":"","APIConnect":"DONE","validated_on":"2023-08-28 19:16:51","gw_version":"","offer_avail":1,"card_ref_id":"dc1da4f52669828139e81ef5eb0f48a5a99ea054a131e00a562887d455417dd919","isTokeizeSuccess":0,"campaign_code":""}
