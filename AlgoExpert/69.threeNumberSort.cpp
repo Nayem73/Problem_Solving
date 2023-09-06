2d5653a8203cb0ca132837bd828b85991f416a80
Nayem73 <nayem73@gmail.com>
2023-09-06 13:04:16 +0600

    Alhamdulillah it worked the algoexpert way

---
+++ AlgoExpert/69.threeNumberSort.cpp
@@ -0,0 +1,67 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	vector<int> nums{1,0,0,-1,-1,0,1,1};
+	vector<int> order{0,1,-1};
+
+	int n = nums.size();
+
+	int f = 0, s = 0, t = n-1;
+
+	while (s >= f && t >= s) {
+		rje()<<rji(f) rji(s) rji(t) rji(nums);
+		if (nums[s] == order[0]) {
+			swap(nums[s], nums[f]);
+			f++; s++;
+		} else if (nums[s] == order[2]) {
+			swap(nums[s], nums[t]);
+			t--;
+		} else {
+			s++;
+		}
+		rje()<<rji(f) rji(s) rji(t) rji(nums) << endl;
+	}
+
+	rje()<<rji(nums);
+}
