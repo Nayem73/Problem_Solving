b3c00880a0d5fe72409ca030c3232c1d92a30472
nayem73 <nayem73@gmail.com>
2023-11-30 20:15:45 +0600

    Alhamdulillahi Rabbil Alamin it was accepted with 1e8 complexity

---
+++ others/spoj_Prime1.cpp
@@ -0,0 +1,85 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+vector<int> store;
+vector<bool> primes(40003);
+
+void sieve(int n) {
+	for (int i = 2; i <= n; i++) {
+		if (primes[i]) continue;
+		store.push_back(i);
+		for (int j = i+i; j <= n; j+=i) {
+			primes[j] = true;
+		}
+	}
+}
+
+bool isPrime(int n) {
+	for (int p : store) {
+		if (p > sqrt(n)) break;
+		if (n % p == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void test_case() {
+	int n, m; cin >> n >> m;
+	for (int i = n; i <= m; i++) {
+		if (i == 1) continue;
+		if (isPrime(i)) {
+			cout << i << endl;
+		}
+	}
+	cout << endl;
+}
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	sieve(4e4);
+	cerr << store.size() << endl;
+
+	int t; cin >> t; while (t--) {
+		test_case();
+	}
+}
