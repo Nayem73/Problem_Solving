e41ca562e2d4ac53b35dc18322660d64f9fb3bc0
nayem73 <nayem73@gmail.com>
2024-01-05 07:51:23 +0600

    Alhamdulillah Atcoder Dp after long time

---
+++ AtCoder/F.LCS.cpp
@@ -0,0 +1,69 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	string s, t;
+	cin >> s >> t;
+	int n = s.size(), m = t.size();
+
+	vector<vector<int>> dp(n, vector<int> (m));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			dp[i][j] = max(dp[i][j], (i?dp[i-1][j]:0));
+			dp[i][j] = max(dp[i][j], (j?dp[i][j-1]:0));
+
+			if (s[i] == t[j]) {
+				dp[i][j] = max(dp[i][j], (i&&j?dp[i-1][j-1]:0) + 1);
+			}
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			ans = max(ans, dp[i][j]);
+		}
+	}
+	cout << ans << endl;
+}
