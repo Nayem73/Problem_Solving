20b4a5296b92e5a3d127d8e7c25f19cf0c9a55fa
nayem73 <nayem73@gmail.com>
2024-05-24 23:05:47 +0600

    bjet substring with condition sliding window

---
+++ others/bjet-substring-with-condition-sliding-window.cpp
@@ -0,0 +1,73 @@
+//BISMILLAHIR-RAHMANIR-RAHIM//
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > rje & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, rje&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct rje {
+#ifdef LOCALM
+~rje() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define rji(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fast_io {ios_base::sync_with_stdio(0); cin.tie(0);}
+#define endl '\n'
+
+int main() {
+//ALHAMDULILLAHI-RABBIL-ALAMIN//
+#ifdef LOCALM
+freopen("in1", "r", stdin);
+freopen("out1", "w", stdout);
+#endif
+fast_io;
+//-------------------------------	
+	//SUBHANALLAH//
+//-------------------------------
+	string s = "aaaccccddddeeefff";
+	int n = s.size();
+	
+	int ans = 0;
+	string ansString;
+
+	int lo = 0, hi = 0;
+	map<char, int> map;
+	while (hi < n) {
+		map[s[hi]]++;
+		while (map.size() > 2) {
+			map[s[lo]]--;
+			if (map[s[lo]] == 0) {
+				map.erase(s[lo]);
+			}
+			lo++;
+		}
+		if (hi-lo+1 >= ans) {
+			ans = hi-lo+1;
+			ansString.erase();
+			for (int i = lo; i <= hi; i++) {
+				ansString += s[i];
+			}
+		}
+		hi++;
+	}
+
+	cerr << ansString << endl;
+}
