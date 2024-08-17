8a3ad3f7051205494d3e21578a397b92d4f6d695
nayem73 <nayem73@gmail.com>
2024-08-17 23:40:45 +0600

    address of ara

--- others/c-prac1.cpp
+++ others/c-prac1.cpp
@@ -4,7 +4,8 @@ int main() {
 	freopen("in1", "r", stdin);
 	freopen("out1", "w", stdout);
 	
-	for (int i = 0; i <= 129; i++) {
-		printf("ASCII for %c is: %d\n", i, i);
-	}
+	int ara[5] = {50, 60, 70, 80, 90};
+
+	printf("Address of ara is %p\n", ara); //this also works &ara
+	printf("Address of ara[3] is %p", &ara[3]);
 }
