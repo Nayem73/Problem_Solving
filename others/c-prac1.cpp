064423a7c31924356fcbf0d566450398144c33ea
nayem73 <nayem73@gmail.com>
2024-08-18 00:40:49 +0600

    tricky pointer

--- others/c-prac1.cpp
+++ others/c-prac1.cpp
@@ -4,11 +4,16 @@ int main() {
 	freopen("in1", "r", stdin);
 	freopen("out1", "w", stdout);
 
-	printf("%d\n", sizeof(int));
+	int x = 10;
+	
+	int *ptr;
+	ptr = &x;
 
-	int num;
-	printf("%d\n", sizeof(num));
+	*ptr += 3;
 
-	int num2 = 55;
-	printf("%d\n", sizeof(num2));
+	printf("%d\n", x);
+
+	x = 5;
+
+	printf("%d\n", x);
 }
