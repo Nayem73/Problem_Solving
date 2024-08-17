52f34efa41a49bb000296a292f479b38a2005b7a
nayem73 <nayem73@gmail.com>
2024-08-17 23:53:35 +0600

    using sizeof(int) to get the size of int data type

--- others/c-prac1.cpp
+++ others/c-prac1.cpp
@@ -3,9 +3,12 @@
 int main() {
 	freopen("in1", "r", stdin);
 	freopen("out1", "w", stdout);
-	
-	int ara[5] = {50, 60, 70, 80, 90};
 
-	printf("Address of ara is %p\n", ara); //this also works &ara
-	printf("Address of ara[3] is %p", &ara[3]);
+	printf("%d\n", sizeof(int));
+
+	int num;
+	printf("%d\n", sizeof(num));
+
+	int num2 = 55;
+	printf("%d\n", sizeof(num2));
 }
