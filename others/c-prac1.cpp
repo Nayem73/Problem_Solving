678522efe47d736718c8d2c25773753171937734
nayem73 <nayem73@gmail.com>
2024-08-18 08:24:47 +0600

    print array using pointer. You can also print string similarly

--- others/c-prac1.cpp
+++ others/c-prac1.cpp
@@ -5,20 +5,23 @@ int main() {
 	freopen("out1", "w", stdout);
 
 	
-	// int yt[] = {4,38,2};
+	int yt[] = {4,38,2};
 
-	// // for (int i = 0; i < n; i++) {
-	// // 	scanf("%d", &yt[i]);
-	// // }
+	// for (int i = 0; i < n; i++) {
+	// 	scanf("%d", &yt[i]);
+	// }
 
 	// for (int i = 0; i < 3; i++) {
 	// 	printf("%d\n", yt[i]);
 	// }
 
+	int *p = yt;
+	printf("%d\n", p[2]);
 
 
-	char s[] = "Bangla";
 
-	printf("%s\n", s);
-	printf("%p\n", s); //&s also works
+	// char s[] = "Bangla";
+
+	// printf("%s\n", s);
+	// printf("%p\n", s); //&s also works
 }
